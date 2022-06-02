71eb3df77bb3390ff476e0f0294440d062a832b1
Monica Alejandra Restrepo Moreno <monicareestrepo123@gmail.com>
2022-06-02 11:37:28 -0500

    Agregue codigo al archivo js

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # practicas-javascript
+
+## Taller 1 figuras geometricas 
+
+
+
+-paso 1: definir las formulas
+
+-paso 2: implementar las formulas en javascript 
+
+-paso 3: crear funcion
+
+-paso 4: integrar js con html
+
--- figuras.js
+++ figuras.js
@@ -1,1 +1,27 @@
-console.log("Hola moni")
+//código del cuadrado
+console.group("Cuadrado");
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cm");
+
+const perimetrocuadrado = ladoCuadrado * 4 ;
+
+console.log("El Perimetro del cuadrado es: " + perimetrocuadrado + "cm");
+
+const areacuadrado = ladoCuadrado* ladoCuadrado;
+
+console.log("El area del cuadrado es: " + areacuadrado + "cm^2");
+
+//código del triángulo
+console.group("Triángulo");
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const basetriangulo = 4;
+
+console.log("Los lados del triángulo miden: "
+  + ladoTriangulo1
+  + "cm,"
+  + ladoTriangulo2 
+  + "cm,"+ basetriangulo 
+  + "cm"
+  );
