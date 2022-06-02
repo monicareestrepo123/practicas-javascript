9aae5a2239248946a4c04d249ca563f19c807d82
Monica Alejandra Restrepo Moreno <monicareestrepo123@gmail.com>
2022-06-02 11:59:13 -0500

    Se agrego mas codigo al js

--- figuras.js
+++ figuras.js
@@ -18,6 +18,7 @@ const ladoTriangulo1 = 6;
 const ladoTriangulo2 = 6;
 const basetriangulo = 4;
 
+
 console.log("Los lados del triángulo miden: "
   + ladoTriangulo1
   + "cm,"
@@ -25,3 +26,43 @@ console.log("Los lados del triángulo miden: "
   + "cm,"+ basetriangulo 
   + "cm"
   );
+
+  const alturatriangulo = 5.5;
+
+  console.log("La altura del trángulo es de: "
+   + alturatriangulo
+   + "cm");
+
+   const perimetrotriangulo = ladoTriangulo1 +
+   ladoTriangulo2 + basetriangulo;
+
+   console.log("El perimetro del triángulo es: "
+   + perimetrotriangulo
+   + "cm");
+
+   const areatriangulo = (basetriangulo * alturatriangulo) / 2 ;
+   console.log("El area del triángulo es: " + areatriangulo + "cm^2");
+
+   console.groupEnd();
+
+//código del circuloos
+console.group("Circulos");
+//radio
+const radiocirculo = 4;
+console.log("El radio del circulo es: " + radiocirculo + "cm");
+//diametro
+const diametrocirculo = radiocirculo * 2;
+console.log("El diametro del circulo es: " + diametrocirculo + "cm");
+//PI
+const PI = Math.PI;
+console.log("El PI del circulo es: " + PI + "cm");
+//circunferencia
+const perimetrocirculo = diametrocirculo * PI;
+console.log("El perimetro del circulo es: " + perimetrocirculo + "cm");
+//area
+const areacirculo = (radiocirculo * radiocirculo) * PI;
+console.log("El area del circulo es: " + areatriangulo + "cm");
+
+
+console.groupEnd();
+
